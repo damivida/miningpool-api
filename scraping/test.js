87f92f42f1c7ffd7c83cd1a2c9966dffa566aa93
Damir Vidaković <vida.damir@gmail.com>
2020-09-16 11:57:51 +0200

    added dash crawler

--- scraping/test.js
+++ scraping/test.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const profRound = require('./profRound');
 
 //F2POOL
 async function f2poolBTC() {
@@ -88,4 +89,101 @@ async function coinWarzBCH() {
 
 }
 
-coinWarzBCH();
+//coinWarzBCH();
+
+// Dash minining
+
+async function miningPoolHubDASH() {
+
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto('https://dash.miningpoolhub.com/index.php?page=statistics&action=pool');
+    const html = await page.content();
+    const $ = cheerio.load(html);
+
+    const poolName = 'Mining Pool Hub - DASH';
+    let lastBlockTime = $('#main > div:nth-child(2) > article:nth-child(2) > div > table > tbody > tr:nth-child(8) > td').text();
+    let hp = parseFloat($('#main > div:nth-child(2) > article:nth-child(1) > div > table > tbody > tr:nth-child(1) > td:nth-child(4)')
+      .text()
+      .replace(',', '')
+      .replace(',', '')
+      .replace(',', ''));
+
+    let coinsPerDay = parseFloat($('#main > div:nth-child(2) > article:nth-child(1) > div > table > tbody > tr:nth-child(1) > td:nth-child(5)').text());
+    let prof = (coinsPerDay / hp) * 1000;
+    let profitability = profRound(prof)
+    let url = 'https://dash.miningpoolhub.com/index.php?page=statistics&action=pool';
+
+    console.log({poolName, hp, coinsPerDay, profitability, lastBlockTime, url});
+
+    return ({ poolName, hp, coinsPerDay, profitability, lastBlockTime, url });
+
+  }
+
+ // miningPoolHubDASH()
+
+  //----
+  async function coinotronDASH() {
+
+    try {
+
+     const browser = await puppeteer.launch({ headless: true });
+     const page = await browser.newPage();
+
+      await page.setDefaultNavigationTimeout(0);
+      await page.goto('https://www.coinotron.com/app?action=statistics');
+      const html = await page.content();
+      const $ = cheerio.load(html);
+
+      const poolName = 'Coinotron - DASH';
+      const lastBlockTime = $('#row0TableBestMinersDRK > td:nth-child(4)').text();
+      let hp = parseFloat($('#row0TableBestMinersDRK > td:nth-child(3)').text().replace(' GH', ''));
+      let coinsPerDay = parseFloat($('#row0TableBestMinersDRK > td:nth-child(4)').text());
+      let prof = (coinsPerDay / hp) / 1000;
+      let profitability = profRound(prof)
+      let url = 'https://www.coinotron.com/app?action=statistics';
+
+
+      console.log({ poolName, hp, coinsPerDay, profitability, lastBlockTime, url });
+      return ({ poolName, hp, coinsPerDay, profitability, lastBlockTime, url });
+
+    } catch (err) {
+      console.log(err);
+
+    }
+
+  }
+  //coinotronDASH();
+
+    //F2POOL DASH
+    async function f2poolDASH() {
+        try {
+    
+          const browser = await puppeteer.launch({headless:true});
+          const page = await browser.newPage(); 
+    
+          await page.setDefaultNavigationTimeout(0);
+          await page.goto('https://www.f2pool.com/');
+          const html = await page.content();
+          const $ = cheerio.load(html);
+    
+    
+          const poolName = 'F2Pool';
+          const profWithFee = parseFloat($("#tab-content-main > table > tbody > tr:nth-child(18) > td > div > div > div.container-info.col-12.col-lg-6 > div.row.calc-inline.hash-val-container > div > div:nth-child(4) > span.pl-1.profit-val.info-value").text().trim());
+          const fee = parseFloat($("#tab-content-main > table > tbody > tr:nth-child(18) > td > div > div > div.container-info.col-12.col-lg-6 > div.row.info-content > div:nth-child(10) > span.info-value").text().replace("% PPS", ""));
+          let profitability = parseFloat((profWithFee / ((100 - fee) / 100)).toFixed(8));
+          profitability =  parseFloat((profitability/1000).toFixed(8));
+          const url = 'https://www.f2pool.com/';
+    
+          console.log({poolName, profWithFee, fee, profitability, url});
+          return ({ poolName, profWithFee, fee, profitability, url });
+    
+        } catch (error) {
+          console.error(error);
+        }
+    
+    }     
+
+f2poolDASH();
--- src/server.js
+++ src/server.js
@@ -139,7 +139,7 @@ app.get('/miningPools/eth/crawler', (req, res) => {
       const poolName = 'F2Pool';
       const profWithFee = parseFloat($("#tab-content-main > table > tbody > tr:nth-child(10) > td > div > div > div.container-info.col-12.col-lg-6 > div.row.calc-inline.hash-val-container > div > div:nth-child(4) > span.pl-1.profit-val.info-value").text().trim());
       const fee = parseFloat($('#tab-content-main > table > tbody > tr:nth-child(10) > td > div > div > div.container-info.col-12.col-lg-6 > div.row.info-content > div:nth-child(8) > span.info-value').text().replace('% PPS+', ''));
-      const profitability = parseFloat((profWithFee / ((100 - 2.5) / 100)).toFixed(8));
+      const profitability = parseFloat((profWithFee / ((100 - fee) / 100)).toFixed(8));
       const url = 'https://www.f2pool.com/';
 
       //console.log({profWithFee, fee, profitability});
@@ -858,6 +858,133 @@ app.get('/miningPools/DASH', (req, res) => {
 });
 
 //---------------------------------------------- Dash scraping ---------------------------
+
+app.get('/miningPools/dash/crawler', (req, res) => {
+
+  async function miningPoolHubDASH(page) {
+
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto('https://dash.miningpoolhub.com/index.php?page=statistics&action=pool');
+    const html = await page.content();
+    const $ = cheerio.load(html);
+
+    const poolName = 'Mining Pool Hub - DASH';
+    let lastBlockTime = $('#main > div:nth-child(2) > article:nth-child(2) > div > table > tbody > tr:nth-child(8) > td').text();
+    let hp = parseFloat($('#main > div:nth-child(2) > article:nth-child(1) > div > table > tbody > tr:nth-child(1) > td:nth-child(4)')
+      .text()
+      .replace(',', '')
+      .replace(',', '')
+      .replace(',', ''));
+
+    let coinsPerDay = parseFloat($('#main > div:nth-child(2) > article:nth-child(1) > div > table > tbody > tr:nth-child(1) > td:nth-child(5)').text());
+    let prof = (coinsPerDay / hp) * 1000;
+    let profitability = profRound(prof)
+    let url = 'https://dash.miningpoolhub.com/index.php?page=statistics&action=pool';
+
+    //console.log({poolName, hp, coinsPerDay, profitability, lastBlockTime});
+
+    return ({ poolName, hp, coinsPerDay, profitability, lastBlockTime, url });
+
+  }
+
+  //COINOTRON
+  async function coinotronDASH(page) {
+
+    try {
+
+      await page.setDefaultNavigationTimeout(0);
+      await page.goto('https://www.coinotron.com/app?action=statistics');
+      const html = await page.content();
+      const $ = cheerio.load(html);
+
+      const poolName = 'Coinotron - DASH';
+      const lastBlockTime = $('#row0TableBestMinersDRK > td:nth-child(4)').text();
+      let hp = parseFloat($('#row0TableBestMinersDRK > td:nth-child(3)').text().replace(' GH', ''));
+      let coinsPerDay = parseFloat($('#row0TableBestMinersDRK > td:nth-child(4)').text());
+      let prof = (coinsPerDay / hp) / 1000;
+      let profitability = profRound(prof)
+      let url = 'https://www.coinotron.com/app?action=statistics';
+
+
+    //  console.log({ poolName, hp, coinsPerDay, profitability, lastBlockTime, url });
+      return ({ poolName, hp, coinsPerDay, profitability, lastBlockTime, url });
+
+    } catch (err) {
+      console.log(err);
+
+    }
+
+  }
+
+  //F2POOL
+  async function f2poolDASH(page) {
+    try {
+
+      await page.setDefaultNavigationTimeout(0);
+      await page.goto('https://www.f2pool.com/');
+      const html = await page.content();
+      const $ = cheerio.load(html);
+
+
+      const poolName = 'F2Pool - DASH';
+      const profWithFee = parseFloat($("#tab-content-main > table > tbody > tr:nth-child(18) > td > div > div > div.container-info.col-12.col-lg-6 > div.row.calc-inline.hash-val-container > div > div:nth-child(4) > span.pl-1.profit-val.info-value").text().trim());
+      const fee = parseFloat($("#tab-content-main > table > tbody > tr:nth-child(18) > td > div > div > div.container-info.col-12.col-lg-6 > div.row.info-content > div:nth-child(10) > span.info-value").text().replace("% PPS", ""));
+      let profitability = parseFloat((profWithFee / ((100 - fee) / 100)).toFixed(8));
+      profitability =  parseFloat((profitability/1000).toFixed(8));
+      const url = 'https://www.f2pool.com/';
+
+      console.log({poolName, profWithFee, fee, profitability, url});
+      return ({ poolName, profWithFee, fee, profitability, url });
+
+    } catch (error) {
+      console.error(error);
+    }
+
+}  
+
+
+  const scrapingDASH = async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    //profitability calc
+    let arrProf = [];
+    const getSum = (total, numb) => {
+      return total + numb;
+    }
+
+    let scrapMiningPoolHubDASH = await miningPoolHubDASH(page);
+    let profMiningPoolHubDASH = scrapMiningPoolHubDASH.profitability;
+    arrProf.push(profMiningPoolHubDASH)
+
+    let scrapCoinotronDASH = await coinotronDASH(page);
+    let profCoinotronDASH = scrapCoinotronDASH.profitability;
+    arrProf.push(profCoinotronDASH)
+
+    let scrapingF2PoolDASH = await f2poolDASH(page);
+    let profF2PoolDASH = scrapingF2PoolDASH.profitability;
+    arrProf.push(profF2PoolDASH);
+
+
+    let profAvg = arrProf.reduce(getSum);
+    profAvg = parseFloat((profAvg / arrProf.length).toFixed(8));
+
+    //--------------------------------
+
+    const dashMiningPools = {
+      miningPoolHubDASH: scrapMiningPoolHubDASH,
+      coinotronDASH: scrapCoinotronDASH,
+      f2PoolDASH: scrapingF2PoolDASH,
+      avgDASHProf: profAvg
+    }
+   
+    res.send({ dashMiningPools });
+   
+  }
+
+  scrapingDASH();
+
+});
 
 
 //**********************************************ZEC mining pools**********************************
